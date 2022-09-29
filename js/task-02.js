3c3849e17a1f71c02e15aa3d37bee09291bce312
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-09-29 10:40:12 +0300

    add second task

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
+
+const ingredientsRef = ingredients.map(elem => {
+  const liRef = document.createElement("li");
+  liRef.textContent = elem;
+  liRef.classList.add("item");
+  return liRef;
+});
+
+
+const listRef = document.querySelector("#ingredients");
+listRef.append(...ingredientsRef);
+console.log(listRef);
