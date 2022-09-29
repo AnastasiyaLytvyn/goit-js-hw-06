60290ad8f7201d18cb20914e475e415ff7b51c6b
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-09-29 15:47:37 +0300

    add fourth task

---
+++ js/task-04.js
@@ -0,0 +1,6 @@
+const counterValue = 0;
+
+const decRef = document.querySelector('[data-action="decrement"]');
+const incRef = document.querySelector('[data-action="increment"]');
+const valRef =document.querySelector("#value")
+
