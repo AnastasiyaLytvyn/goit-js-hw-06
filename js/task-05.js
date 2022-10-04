d98b7fa41b8982a264015bc2b9b90d2a1c9ac429
AnastasiyaLytvyn <n8stusikk@gmail.com>
2022-10-04 19:30:38 +0300

    update task

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,12 @@
-const inputRef = document.querySelector('#name-input');
-const outputRef = document.querySelector('#name-output');
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
 
-inputRef.addEventListener('input', onInputChange);
+inputRef.addEventListener("input", onInputChange);
 
-
-function onInputChange (event) {
-    console.log(event.currentTarget.value);
-    outputRef.textContent = event.currentTarget.value;
-};
+function onInputChange() {
+  if (inputRef.value) {
+    outputRef.textContent = inputRef.value;
+  } else {
+    outputRef.textContent = "Anonymous";
+  }
+}
