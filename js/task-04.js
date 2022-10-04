40abbc4c6a6321d19fa2551b7e5919dfe3fc57bb
AnastasiyaLytvyn <n8stusikk@gmail.com>
2022-10-04 10:30:57 +0300

    update task

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,17 @@
-const counterValue = 0;
+let counterValue = 0;
 
 const decRef = document.querySelector('[data-action="decrement"]');
 const incRef = document.querySelector('[data-action="increment"]');
 const valRef =document.querySelector("#value")
 
+
+decRef.addEventListener('click', (event) => {
+    counterValue -= 1;
+    valRef.textContent = counterValue;
+
+});
+
+incRef.addEventListener('click', (event) => {
+    counterValue += 1;
+    valRef.textContent = counterValue;
+})
